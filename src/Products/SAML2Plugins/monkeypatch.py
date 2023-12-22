54019e4a04fb920e75eed6d0cf7564fb7d24d790
Jens Vagelpohl <jens@plyp.com>
2023-12-22 14:12:25 +0100

    - try to force new schema into pysaml2

--- src/Products/SAML2Plugins/monkeypatch.py
+++ src/Products/SAML2Plugins/monkeypatch.py
@@ -14,6 +14,7 @@
 """
 
 import logging
+import os
 
 
 logger = logging.getLogger('Products.SAML2Plugins')
@@ -38,6 +39,33 @@ def pysaml2_add_signature_support():
     entity.SIG_ALLOWED_ALG = xmldsig.SIG_ALLOWED_ALG
 
 
+def pysaml_add_xml_schemata():
+    from saml2.xml import schema
+
+    # pysaml2 has a hardcoded list of supperted XML schemata and namespaces,
+    # which does not include newer versions.
+    OLD_create_xml_schema_validator = schema._create_xml_schema_validator
+
+    def create_custom_validator(source, **kwargs):
+        parent_folder_path = os.path.dirname(os.path.abspath(__file__))
+        schema_data_folder = os.path.join(parent_folder_path, 'data')
+        additional_schemata = (
+            ('http://www.w3.org/2009/xmlenc11#', 'xenc-schema-11.xsd'),
+        )
+
+        xml_schema = OLD_create_xml_schema_validator(source, kwargs)
+        for namespace, filename in additional_schemata:
+            schema_path = os.path.join(schema_data_folder, filename)
+            xml_schema.include_schema(location=namespace,
+                                      base_url=schema_path,
+                                      build=True)
+
+        return xml_schema
+
+    schema._create_xml_schema_validator = create_custom_validator
+
+
 def applyPatches():
     logger.debug('Applying monkey patches')
     pysaml2_add_signature_support()
+    pysaml_add_xml_schemata()
